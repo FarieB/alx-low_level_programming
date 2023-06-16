8c5072b6c04bee4df39c5638228697cc6eb1af08
FaraiBande <faraibande@gmail.com>
2023-06-16 14:36:26 -0700

    Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: (printing alphabet in upper case)
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int i;
+
+
+	for (i = 0; i < 52; i++)
+
+	{
+
+		putchar(alp[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,5 @@
 alx-low_level_programming
 random numbers to variable
 Prints the last digit of a randomly generated number
+print alphabet in lower case
+print alphabet in upper case
