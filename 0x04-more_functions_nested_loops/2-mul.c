4fe20066dbfee70f745c0a85e056d1a517081141
FaraiBande <faraibande@gmail.com>
2023-06-23 03:09:04 -0700

    Write a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two digits
+ * @a: first integer
+ * @b: second integer
+ * Description: function that multiplies two digits
+ * Return: return the result of multiplying two digits
+ */
+
+int mul(int a, int b)
+{
+	int outp;
+
+	outp = a * b;
+
+	return (outp);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0x04. C - More functions, more nested loops
 1-isdigit.c | Write a function that checks for a digit (0 through 9).
+2-mul.c | Write a function that multiplies two integers.
