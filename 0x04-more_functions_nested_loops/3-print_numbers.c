ba5bd86502d9c1d7c0232621548912645cd1a59a
FaraiBande <faraibande@gmail.com>
2023-06-23 03:29:40 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints numbers from 0 - 9
+ * Description: a function that prints the numbers, from 0 to 9.
+ * Return: number from 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0x04. C - More functions, more nested loops
 1-isdigit.c | Write a function that checks for a digit (0 through 9).
 2-mul.c | Write a function that multiplies two integers.
+3-print_numbers.c | Write a function that prints the numbers, from 0 to 9, followed by a new line.
