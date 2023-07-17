3455e060c83635666072ffb205cd44576fbc1264
FaraiBande <faraibande@gmail.com>
2023-07-17 08:12:50 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+  * header file that defines a macro named SIZE 
+  * as an abbreviation for the token 1024
+  */
+
+#define SIZE 1024
+
+#endif
