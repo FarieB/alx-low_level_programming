491867642421a0dc2df27265374d54e2eaf2a28b
FaraiBande <faraibande@gmail.com>
2023-07-19 11:34:15 -0700

    BOB DYLAN

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints the name
+ * @name: the name
+ * @f: the function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
