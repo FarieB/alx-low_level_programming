b7ccb1eb88c826a3f1bf393050377b069b6aa4a8
FaraiBande <faraibande@gmail.com>
2023-07-20 03:58:10 -0700

    Django

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - function that prints strings
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ * Return: NULL
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+	va_list strings;
+	char *strng;
+	unsigned int index;
+
+	va_start(strings, n);
+
+	for (index = 0; index < n; index++)
+	{
+		strng = va_arg(strings, char *);
+
+		if (strng == NULL)
+			printf("(nil)");
+		else
+			printf("%s", strng);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(strings);
+}
+
